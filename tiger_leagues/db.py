5a446d121d33c7e30c6526118a5f803b2a3f11cf
Rui De Oliveira <ruideoliveira@nat-oitwireless-inside-vapornet100-c-13741.Princeton.EDU>
2018-11-14 21:56:35 -0500

    updated database program

--- tiger_leagues/db.py
+++ tiger_leagues/db.py
@@ -23,25 +23,25 @@ def disconnect(self):
     self._connection.close()
 
 def launch(self):
-	stmtstr1 = 'CREATE TABLE League_Info (LeagueId int, LeagueName varchar(255), Deadline date)'
-
-	stmtstr2 = 'CREATE TABLE Player_Info (UserId int, LeagueId int, Name varchar(255), ' + \
-				'NetId varchar(255), Console int, Room varchar(255), Status varchar(255)' 
+	stmtstr = 'CREATE TABLE Player_Accounts (UserId int, Name varchar(255), NetId varchar(255), Email varchar(255) ' + \
+				'PhoneNum varchar(255), Room varchar(255), LeagueId varchar(255)' 
 
 
-	stmtstr3 = 'CREATE TABLE Match_Info (LeagueId int, MatchId int, User1 int, User2 int, Score1 int, ' + \
+	stmtstr2 = 'CREATE TABLE Match_Info (LeagueId int, MatchId int, User1 int, User2 int, Score1 int, ' + \
 				'Score2 int, Deadline date)'
+
+	stmtstr3 = 'CREATE TABLE League_Info (LeagueName varchar(255), Descrip varchar(255), WinPoints int, ' + \
+				'DrawPoints int, LossPoints int, LeagueId int,'
 
 	cursor = self._connection.cursor()
 	cursor.execute(stmtstr1)
 	cursor.execute(stmtstr2)
-	cursor.execute(stmtstr3)
 
 # leagueinfo must be a list, dict or tuple that contains info from a form
 # knowing the particularily order of the elements in the tuple is VERY IMPORTANT
 
 def isMember(self, netid)
-	stmtstr1 = 'SELECT UserId FROM Player_Info WHERE NetId = netid'
+	stmtstr1 = 'SELECT UserId FROM Player_Accounts WHERE NetId = netid'
 	cursor = self._connection.cursor()
 	cursor.execute(stmtstr1)
 	userid = cursor.fetchone()
@@ -52,18 +52,20 @@ def isMember(self, netid)
 		return 1
 
 def createProfile(self, playerinfo):
-	stmtstr1 = 'SELECT max(UserId) FROM Player_Info'
+	stmtstr1 = 'SELECT max(UserId) FROM Player_Accounts'
 	cursor = self._connection.cursor()
 	cursor.execute(stmtstr1)
 	userid = cursor.fetchone()
 
 	userid = userid + 1
 
-	# playerinfo should be a tuple that contains the player's name, netid, console situation, room, and league status
-	stmtstr2 = 'INSERT INTO Player_Info (UserId, Name, NetId, Console, Room, Status) ' +\
-				'VALUES (userid, playerinfo[0], playerinfo[1], playerinfo[2], playerinfo[3], Leagueless'
+	accountinfo = [userid, playerinfo["Name"], playerinfo["NetId"], playerinfo["Email"], playerinfo["PhoneNum"], playerinfo["Room"]]
 
-	cursor.execute(stmtstr2)
+	# playerinfo should be a tuple that contains the player's name, netid, console situation, room, and league status
+	stmtstr2 = 'INSERT INTO Player_Accounts (UserId, Name, NetId, Email, PhoneNum, Room) ' +\
+				'VALUES (?, ?, ?, ?, ?, ?)'
+
+	cursor.execute(stmtstr2, accountinfo)
 
 
 def createLeague(self, leagueinfo):
@@ -77,15 +79,42 @@ def createLeague(self, leagueinfo):
 
 	leaguid = leagueid + 1
 
-	stmtstr2
+	userid = leagueinfo("UserId")
+
+	leaguebasics = [leagueid, leagueinfo["LeagueName"], leagueinfo["Descrip"], leagueinfo["WinPoints"], leagueinfo["DrawPoints"], leagueinfo["LossPoints"]]
+
+	stmtstr2 = 'INSERT INTO League_Info (LeagueId, LeagueName, Descrip, WinPoints, DrawPoints, LossPoints) ' + \
+				'Values (?, ?, ?, ?, ?, ?)'
+
+	cursor.execute(stmtstr2, leaguebasics)
+
+	# questions provided by the creator of league, given as the keys in leagueinfo
+	temp = leagueinfo.keys()
+
+	requests = []
+
+	stmtstr3 = 'CREATE TABLE '
+
+	stmtstr3 = stmtstr3 + leagueid + " (UserId, Status"
+
+	for i in range(6, len(leagueinfo)):
+		requests.append(leagueinfo[temp[i]])
+		stmtstr3 = stmtstr3 + ", " + temp[i]
+
+	stmtstr3 = ") Values (" + userid + ", Admin"
+
+	for i in range(2, len(requests)):
+			stmtstr3 = stmtstr3 + ", " + requests[i]
+
+	stmtstr3 = stmtstr3 + ")"
+
+	cursor.execute(stmtstr3)
+
+def joinLeague(self, playerinfo):
 
 
-
-
-def joinLeague(self, leagueid):
-	
-
-
+def getRequests(self, leagueid):
+	stmtstr = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS"
 
 def leaveLeague(self):
 
