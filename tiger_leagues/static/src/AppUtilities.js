4e33eebc911cba44c4aa705cdcafc23e8813746b
dchege711 <d.chege711@gmail.com>
2018-11-15 22:24:19 -0500

    Generalize the HTTPRequest method. Return entire response

--- tiger_leagues/static/src/AppUtilities.js
+++ tiger_leagues/static/src/AppUtilities.js
@@ -5,22 +5,18 @@
  * @param {string} method The method to use, e.g. `POST`
  * @param {string} url The URL that the request will be sent to
  * @param {JSON} payload The data that will be sent along with the request
+ * @returns {Promise} If successful, the parameter will be the entire response.
  */
-function sendHTTPRequest(method, url, payload) {
+function sendHTTPRequest(method, url, payload, contentType="application/json") {
     return new Promise(function(resolve, reject) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                try {
-                    let json_response_data = JSON.parse(this.responseText);
-                    resolve(json_response_data);
-                } catch (err) { 
-                    reject(err);
-                }
+            if (this.readyState == 4) {
+                resolve(this.response);
             }
         };
         xhttp.open(method, url, true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("Content-Type", contentType);
         xhttp.send(JSON.stringify(payload));
     });
     
