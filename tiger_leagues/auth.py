4709b3843978797064081c0cb0c38e7c094e97af
dchege711 <d.chege711@gmail.com>
2018-11-28 18:06:14 -0500

    Add boilerplate for authentication module

--- tiger_leagues/auth.py
+++ tiger_leagues/auth.py
@@ -18,3 +18,26 @@ def index():
     """
     if request.method == "GET":
         return render_template("/auth/login.html")
+
+"""
+Use CAS to login users. Resources:
+https://www.cs.princeton.edu/courses/archive/fall18/cos333/lectures/14websecurity/PennyBottleCas/
+
+The code written here should be able to set the username and make it available 
+to the rest of the application.
+
+"""
+
+def check_user(username):
+    """
+    If the user is new, render a page that allows the user to submit details 
+    for their account (name, netid, email, phonenumber, room). If the user has 
+    logged in before (i.e. we have them in the database) redirect them to 
+    `/league/`.
+
+    db.py has a getUser() method that should be helpful
+
+    @param `username`: Set to `None` if no valid user has logged in, otherwise 
+    contains the netid of the logged in user.
+    """
+    return None
