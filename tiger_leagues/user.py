69a305e21a75cc89b3d74a564f266cb0810ef0b7
dchege711 <d.chege711@gmail.com>
2018-11-29 14:30:34 -0500

    Add boilerplate for the user module

--- tiger_leagues/user.py
+++ tiger_leagues/user.py
@@ -6,34 +6,77 @@ Exposes a blueprint that handles requests made to `/user/*` endpoint
 """
 
 from flask import Blueprint
-from . import db
+from . import db, decorators
 
 database = db.Database()
 bp = Blueprint("user", __name__, url_prefix="/user")
 
-def getUser(netid):
+def get_user(net_id):
     """
-    @returns `dict` representing a user in the database. If the user doesn't 
-    exist, returns `None`.
+    @returns `dict` representing a user in the database. 
+    
+    @returns `None` If the user doesn't exist.
     """
-    cursor = database.execute(
-        'SELECT UserId FROM Player_Accounts WHERE NetId = %s', values=(netid,)
+    return {
+        "user_id": 0, "name": "Chege Gitau", "net_id": net_id, 
+        "email": "dgitau@princeton.edu", "phone_num": "555-555-5555",
+        "room": "Henry Hall A34", 
+        "associated_leagues": __get_user_league_info_list(0)
+    }
+
+@bp.route("/profile", methods=["GET"])
+@decorators.login_required
+def display_user_profile():
+    """
+    Render a template that contains user information. The user should be able to 
+    request an update some of the displayed information. The userid will be in 
+    the sessions object.
+    
+    Sample information might include:
+
+    Read-Only: NetID
+    Editables: Preferred Name, Preferred Email, Phone Number, Room Number
+    Links to leagues that a user is involved in
+
+    """
+    return "Display user profile"
+
+@bp.route("/update", methods=["POST"])
+@decorators.login_required
+def update_user_profile():
+    """
+    Update the information stored about a user. This method will most likely 
+    receive POST requests from the template rendered by user.displayUserProfile
+
+    """
+    return NotImplementedError()
+
+def __create_user_profile(user_info):
+    """
+    Create a user from the supplied information and save them to the database.
+
+    @returns `Cursor` if transaction is successful.
+
+    """
+
+    return database.execute(
+        (
+            "INSERT INTO users (name, net_id, email, phone_number, room) "
+            "VALUES (%s, %s, %s, %s, %s, %s);"
+        ),
+        values=[
+            user_info["name"], user_info["net_id"], user_info["email"], 
+            user_info["phone_number"], user_info["room"]
+        ]
     )
-    userid = cursor.fetchone()
 
-    return userid
-
-def createProfile(playerinfo):
-    cursor = database.execute('SELECT max(UserId) FROM Player_Accounts')
-    userid = int(cursor.fetchone()[0]) + 1
-
-    accountinfo = [
-        userid, playerinfo["Name"], playerinfo["NetId"], playerinfo["Email"], 
-        playerinfo["PhoneNum"], playerinfo["Room"]
+def __get_user_league_info_list(user_id):
+    """
+    @return `List[dict]` containing all leagues that a user is associated with.
+    
+    """
+    return [
+        {"league_name": "FIFA League", "league_id": 0, "status": "joined"},
+        {"league_name": "Table Tennis", "league_id": 1, "status": "pending"},
+        {"league_name": "Dragonball Z", "league_id": 2, "status": "pending"},
     ]
-
-    # playerinfo should be a tuple that contains the player's name, netid, console situation, room, and league status
-    stmtstr2 = 'INSERT INTO Player_Accounts (UserId, Name, NetId, Email, PhoneNum, Room) ' +\
-            'VALUES (%s, %s, %s, %s, %s, %s)'
-
-    database.execute(stmtstr2, values=accountinfo)
