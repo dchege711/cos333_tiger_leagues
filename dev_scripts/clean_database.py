30ad0d0433ce3e76070a5b010dfe70841b70dea6
dchege711 <d.chege711@gmail.com>
2018-12-12 23:42:14 -0500

    Move clean_database() into a function instead of being a global script

--- dev_scripts/clean_database.py
+++ dev_scripts/clean_database.py
@@ -11,17 +11,26 @@ sys.path.insert(0, "..")
 
 from tiger_leagues import db as database
 
-db = database.Database()
-cursor = db.execute((
-    "SELECT tablename FROM pg_catalog.pg_tables "
-    "WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
-))
+def clean_database():
+    """
+    Delete all the tables in the database.
 
-table_names = [row["tablename"] for row in cursor]
+    @returns `bool`: `True` if the deletion was successful.
+    """
+    db = database.Database()
+    cursor = db.execute((
+        "SELECT tablename FROM pg_catalog.pg_tables "
+        "WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
+    ))
 
-cursor = db.execute(
-    "DROP TABLE {}".format(", ".join(["{}" for _ in table_names])),
-    dynamic_table_or_column_names=table_names
-)
+    table_names = [row["tablename"] for row in cursor]
 
-print("Dropped all the tables")
+    cursor = db.execute(
+        "DROP TABLE {}".format(", ".join(["{}" for _ in table_names])),
+        dynamic_table_or_column_names=table_names
+    )
+
+    return True
+
+if __name__ == "__main__":
+    if clean_database(): print("Dropped all the tables")
