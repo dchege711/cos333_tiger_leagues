97e06e2dc14aa34bf17b1d4e48c5819853586446
Ivy Xue <ixue@princeton.edu>
2019-01-06 23:23:08 -0500

    Adds documentation for exception.py

--- tiger_leagues/models/exception.py
+++ tiger_leagues/models/exception.py
@@ -10,21 +10,19 @@ class TigerLeaguesException(Exception):
     A catchall exception for all problems.
     Status code and message can be customized wherever it's raised.
 
+    :param message: str
+
+    human readable string explaining the problem
+
+    :kwarg status_code: int
+
+    To specify the error code in the response. Like 400, 404, 500, etc. 
+
     """
 
     status_code = 400
 
     def __init__(self, message, status_code=None):
-        """
-        :param message: str
-
-        human readable string explaining the problem
-
-        :kwarg status_code: int
-
-        To specify the error code in the response. Like 400, 404, 500, etc. 
-
-        """
         Exception.__init__(self)
         self.message = message
         if status_code is not None:
