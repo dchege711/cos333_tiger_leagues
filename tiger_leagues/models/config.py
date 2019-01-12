6b884c88163971f2f8df82cd1d9c034e8c151985
dchege711 <d.chege711@gmail.com>
2019-01-12 16:05:54 -0500

    Setup Travis CI testing

--- tiger_leagues/models/config.py
+++ tiger_leagues/models/config.py
@@ -26,6 +26,8 @@ if APP_ENV == "development":
 elif APP_ENV == "production":
     # ... otherwise the app is running on Heroku and the URL is already set
     DATABASE_URL = environ["DATABASE_URL"]
+elif APP_ENV == "travis_ci":
+    DATABASE_URL = "host=localhost dbname=travis_ci_test user=postgres password="
 else:
     raise RuntimeError(
         "Please set the `TIGER_LEAGUES_ENVIRONMENT` to either `development` or `production`"
