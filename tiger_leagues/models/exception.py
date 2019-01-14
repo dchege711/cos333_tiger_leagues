8b4b13255c51be381a1dc800f95e99620b00e10a
dchege711 <d.chege711@gmail.com>
2019-01-14 18:17:45 -0500

    Abstract away the validator into the exception module

--- tiger_leagues/models/exception.py
+++ tiger_leagues/models/exception.py
@@ -5,6 +5,8 @@ Allows for error pages/responses with custom exception messages.
 
 """
 
+import traceback
+
 class TigerLeaguesException(Exception):
     """
     A special exception for errors that arise due to constraints that we set on 
@@ -38,3 +40,36 @@ class TigerLeaguesException(Exception):
             "success": False, "message": self.message, 
             "status": self.status_code
         }
+
+def validate_values(data_obj, constraints, jsonify=True):
+    """
+    Helper function for validating JSON input
+
+    :param data_obj: dict
+
+    A key-value pairing that needs to be validated
+
+    :param constraints: list[tuple]
+
+    Each tuple has 5 items. In order, they are: key (str), 
+    cast_function (function), l_limit (value), u_limit (value), error_msg (str)
+
+    :kwarg jsonify: bool
+
+    If ``True``, the raised ``TigerLeaguesException`` will have its jsonify 
+    attribute set.
+
+    :raises: ``TigerLeaguesException``
+
+    If any of the keys don't exist or any of the values fail to meet the 
+    constraint.
+
+    """
+    for key, cast_function, l_limit, u_limit, error_msg in constraints:
+        try:
+            data_obj[key] = cast_function(data_obj[key])
+            if l_limit is not None: assert data_obj[key] >= l_limit
+            if u_limit is not None: assert data_obj[key] <= u_limit
+        except:
+            traceback.print_exc()
+            raise TigerLeaguesException(error_msg, jsonify=jsonify)
